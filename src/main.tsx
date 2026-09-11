077107b75f4cdbbfd7b7a786dd67150d7fa2efd7
SaluuOG <mtzgfmd7h6@privaterelay.appleid.com>
2026-09-11 21:13:28 +0200

    Wire AuthProvider into Nexus

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { App } from './app/App';
+import { AuthProvider } from './features/auth/AuthProvider';
 import './styles.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </HashRouter>
   </React.StrictMode>,
 );
