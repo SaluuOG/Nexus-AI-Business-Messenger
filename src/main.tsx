6132f92281f0c4ea33779d8b1fbdb756c9c1beef
SaluuOG <mtzgfmd7h6@privaterelay.appleid.com>
2026-09-11 23:55:32 +0200

    Load Nexus contacts styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from './features/auth/AuthProvider';
 import './styles.css';
 import './auth.css';
 import './settings-data.css';
+import './contacts-data.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
